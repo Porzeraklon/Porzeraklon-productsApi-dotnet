d1192956c33aa6d7960535ae54e691c024adc77e
porzeraklon <beldegrin.damian@gmail.com>
2026-05-19 15:51:16 +0200

    Round 2

--- Program.cs
+++ Program.cs
@@ -31,15 +31,20 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    
+    // HttpsRedirection zostaje tylko lokalnie. Na OpenShifcie ruch i tak 
+    // jest szyfrowany przez Router (Ingress) przed dotarciem do kontenera.
+    app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
+// 1. JAWNE URUCHOMIENIE ROUTINGU (Kluczowe dla prawidłowego działania CORS)
+app.UseRouting();
 
-// Użycie CORS
+// 2. URUCHOMIENIE CORS (Musi być PO UseRouting, ale PRZED Autoryzacją i Mapowaniem)
 app.UseCors("AllowAll");
 
+// 3. AUTORYZACJA I MAPOWANIE KONTROLERÓW
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
